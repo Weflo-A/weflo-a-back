880e81b0a319638f5948b73878304259679afa68
whereami0404 <whereami2048@gmail.com>
2024-03-04 21:30:24 +0900

    feat: 교체용 부품 리스트 응답 DTO 구현

--- src/main/java/com/weflo/backend/domain/component/dto/ExchangeComponentResponse.java
+++ src/main/java/com/weflo/backend/domain/component/dto/ExchangeComponentResponse.java
@@ -1,12 +1,13 @@
 package com.weflo.backend.domain.component.dto;
 
+import com.weflo.backend.domain.component.domain.Component;
+import com.weflo.backend.domain.component.domain.ComponentType;
 import com.weflo.backend.domain.component.domain.Part;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
-import org.hibernate.type.ComponentType;
 
 @Builder
-@Getter
 public record ExchangeComponentResponse(
         String image,
         ComponentType type,
@@ -15,4 +16,18 @@ public record ExchangeComponentResponse(
         String description,
         int price
 ) {
+
+
+    public static List<ExchangeComponentResponse> ofList(List<Component> allComponents) {
+        return allComponents.stream().map(component -> builder()
+                .image(component.getImage())
+                .type(component.getType())
+                .part(component.getPart())
+                .name(component.getName())
+                .description(component.getDescription())
+                .price(component.getPrice())
+                .build()
+        ).toList();
+    }
+
 }
