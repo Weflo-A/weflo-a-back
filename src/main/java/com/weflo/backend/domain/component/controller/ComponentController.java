fc0b8fa04c514ac1b860607a3eee91d0b3d17601
whereami0404 <whereami2048@gmail.com>
2024-03-04 21:30:46 +0900

    feat: 교체용 부품 리스트 조회 API 구현

---
+++ src/main/java/com/weflo/backend/domain/component/controller/ComponentController.java
@@ -0,0 +1,41 @@
+package com.weflo.backend.domain.component.controller;
+
+import com.weflo.backend.domain.component.dto.ExchangeComponentResponse;
+import com.weflo.backend.domain.component.service.ComponentService;
+import com.weflo.backend.global.common.SuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/component")
+public class ComponentController {
+
+    private final ComponentService componentService;
+
+    @Operation(
+            summary = "교체용 부품 리스트 조회(전체 부품 조회)",
+            description = "교체용 부품 리스트를 조회합니다."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "get components success"
+    )
+    @GetMapping("/exchange-components")
+    public ResponseEntity<SuccessResponse<?>> getExchangeComponents() {
+        List<ExchangeComponentResponse> exchangeComponentResponses = componentService.getAllComponents();
+
+        return SuccessResponse.ok(exchangeComponentResponses);
+    }
+
+    @GetMapping("/set-data")
+    public void setData() {
+        componentService.setData();
+    }
+}
---
+++ src/main/java/com/weflo/backend/domain/component/service/ComponentService.java
@@ -0,0 +1,68 @@
+package com.weflo.backend.domain.component.service;
+
+import com.weflo.backend.domain.component.domain.Component;
+import com.weflo.backend.domain.component.domain.ComponentType;
+import com.weflo.backend.domain.component.domain.Part;
+import com.weflo.backend.domain.component.dto.ExchangeComponentResponse;
+import com.weflo.backend.domain.component.repository.ComponentRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ComponentService {
+    private final ComponentRepository componentRepository;
+
+    @Transactional(readOnly = true)
+    public List<ExchangeComponentResponse> getAllComponents() {
+        List<Component> allComponents = componentRepository.findAll();
+
+        return ExchangeComponentResponse.ofList(allComponents);
+    }
+
+    @Transactional
+    public void setData() {
+        Component componentA = Component.builder()
+                .name("부품 A")
+                .part(Part.PART1)
+                .price(1000)
+                .description("부품 A 정의")
+                .type(ComponentType.ESC)
+                .image("부품 A 이미지 경로")
+                .build();
+
+        Component componentB = Component.builder()
+                .name("부품 A")
+                .part(Part.PART2)
+                .price(2000)
+                .description("부품 B 정의")
+                .type(ComponentType.BLADE)
+                .image("부품 B 이미지 경로")
+                .build();
+
+        Component componentC = Component.builder()
+                .name("부품 C")
+                .part(Part.PART3)
+                .price(3000)
+                .description("부품 C 정의")
+                .type(ComponentType.MOTOR)
+                .image("부품 C 이미지 경로")
+                .build();
+
+        Component componentD = Component.builder()
+                .name("부품 D")
+                .part(Part.PART1)
+                .price(4000)
+                .description("부품 D 정의")
+                .type(ComponentType.BLADE)
+                .image("부품 D 이미지 경로")
+                .build();
+
+        componentRepository.save(componentA);
+        componentRepository.save(componentB);
+        componentRepository.save(componentC);
+        componentRepository.save(componentD);
+    }
+}
