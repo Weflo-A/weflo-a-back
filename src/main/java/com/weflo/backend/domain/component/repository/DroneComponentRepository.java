1baf26dea51b91ae6ad9b8079e4cc607c27c1431
whereami0404 <whereami2048@gmail.com>
2024-03-04 23:12:59 +0900

    chore: 드론 부품 Repository 패키지 위치 변경

---
+++ src/main/java/com/weflo/backend/domain/component/repository/DroneComponentRepository.java
@@ -0,0 +1,10 @@
+package com.weflo.backend.domain.component.repository;
+
+import com.weflo.backend.domain.drone.domain.Drone;
+import com.weflo.backend.domain.drone.domain.DroneComponent;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DroneComponentRepository extends JpaRepository<DroneComponent, Long> {
+    List<DroneComponent> findByDroneIdAndPointGreaterThanEqual(Long droneId, int point);
+}
--- src/main/java/com/weflo/backend/domain/drone/repository/DroneComponentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.weflo.backend.domain.drone.repository;
-
-import com.weflo.backend.domain.drone.domain.DroneComponent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DroneComponentRepository extends JpaRepository<DroneComponent, Long> {
-}
