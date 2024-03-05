917b4079d6f5d2dc1dd8fdecbb19580babf2a456
whereami0404 <whereami2048@gmail.com>
2024-03-05 15:21:35 +0900

    feat: 그룹별 투입 비용 엔티티 추가

---
+++ src/main/java/com/weflo/backend/domain/cost/domain/DroneGroupMonthCost.java
@@ -0,0 +1,27 @@
+package com.weflo.backend.domain.cost.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class DroneGroupMonthCost {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Long droneCount;
+    private String purpose;
+    private Long monthCost;
+}
