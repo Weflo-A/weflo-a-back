3c9bbbdb77500e19918f2d9f9abc8a4149e69559
whereami0404 <whereami2048@gmail.com>
2024-03-06 14:24:13 +0900

    fix: 드론 그룹 조회 -> 드론 그룹 내 드론 조회 API로 수정

---
+++ src/main/java/com/weflo/backend/domain/drone/controller/DroneGroupController.java
@@ -0,0 +1,25 @@
+package com.weflo.backend.domain.drone.controller;
+
+import com.weflo.backend.domain.drone.dto.response.DroneGroupListResponse;
+import com.weflo.backend.domain.drone.service.DroneGroupService;
+import com.weflo.backend.global.common.SuccessResponse;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class DroneGroupController {
+    private final DroneGroupService droneGroupService;
+
+    @GetMapping("/drone-groups/{droneGroupId}/drones")
+    public ResponseEntity<SuccessResponse<?>> getDronesByGroup(@PathVariable(value = "droneGroupId") Long droneGroupId){
+        List<DroneGroupListResponse> droneGroupListResponses = droneGroupService.getDronesByDroneGroup(droneGroupId);
+        return SuccessResponse.ok(droneGroupListResponses);
+    }
+}
