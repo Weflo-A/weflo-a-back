dde863dacd1b46bb44305c983af3a8d9f4800ff9
Bae Hyeonseo <haulqogustj@naver.com>
2024-03-10 18:24:20 +0900

    54-대시보드 수정

--- src/main/java/com/weflo/backend/domain/drone/dto/response/DroneGroupResponse.java
+++ src/main/java/com/weflo/backend/domain/drone/dto/response/DroneGroupResponse.java
@@ -7,16 +7,17 @@ import lombok.Getter;
 @Builder
 @Getter
 public class DroneGroupResponse {
-    private Long groupId;
+    private String groupId;
     private String name;
     public static DroneGroupResponse of(DroneGroup droneGroup){
         return DroneGroupResponse.builder()
-                .groupId(droneGroup.getId())
+                .groupId(String.valueOf(droneGroup.getId()))
                 .name(droneGroup.getName())
                 .build();
     }
-    public static DroneGroupResponse create(String name){
+    public static DroneGroupResponse create(String groupId,String name){
         return DroneGroupResponse.builder()
+                .groupId(groupId)
                 .name(name)
                 .build();
     }
